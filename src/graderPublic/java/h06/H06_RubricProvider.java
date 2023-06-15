e3b07f76a797a715ae4e272646eb464d5f5e99d4
nyanyan <nhanhuynh_online@protonmail.com>
2023-06-15 14:04:34 +0200

    Finialize rubrics

--- src/graderPublic/java/h06/H06_RubricProvider.java
+++ src/graderPublic/java/h06/H06_RubricProvider.java
@@ -3,10 +3,18 @@ package h06;
 import org.sourcegrade.jagr.api.rubric.Rubric;
 import org.sourcegrade.jagr.api.rubric.RubricProvider;
 
+import static h06.TutorUtils.criterion;
+
 public class H06_RubricProvider implements RubricProvider {
 
     public static final Rubric RUBRIC = Rubric.builder()
         .title("H06")
+        .addChildCriteria(
+            criterion(H1_DirectionVectorTest.class),
+            criterion(H2_WorldTest.class),
+            criterion(H3_MazeSolverRecursiveTest.class),
+            criterion(H4_MazeSolverIterativeTest.class)
+        )
         .build();
 
     @Override
